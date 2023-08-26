e9d3b380f56d838b34eb67899c91ec28c7bf2821
thutasann <66902308+thutasann@users.noreply.github.com>
2023-08-26 19:39:10 +0630

    feat: type guards

--- src/Typescript/TypeGuards.ts
+++ src/Typescript/TypeGuards.ts
@@ -8,7 +8,9 @@
  * and infer types
  */
 
-// 1. typeof guard
+import { printIntrospectionSchema } from 'graphql';
+
+//---------- 1. typeof guard
 type alphanumeric = string | number;
 
 function add(a: alphanumeric, b: alphanumeric) {
@@ -17,3 +19,104 @@ function add(a: alphanumeric, b: alphanumeric) {
   }
 }
 add(3, 4);
+
+//---------- 2. instanceof guard
+class Banana {
+  isTasty(): boolean {
+    return true;
+  }
+}
+class Apple {
+  isJuicy(): boolean {
+    return true;
+  }
+}
+type Fruit = Banana | Apple;
+
+function buyFruit(fruit: Fruit): number {
+  let price = 0;
+  if (fruit instanceof Apple) {
+    price = fruit.isJuicy() ? 5 : 10;
+  }
+  return price;
+}
+const apple = new Apple();
+buyFruit(apple);
+
+//---------- 3. in guard
+class Pizza {
+  isTasty(): boolean {
+    return true;
+  }
+}
+
+class FriedChicken {
+  isGreat(): boolean {
+    return true;
+  }
+}
+
+type FastFood = Pizza | FriedChicken;
+
+function buyFastFood(fastFood: FastFood): number {
+  let price = 0;
+
+  if ('isGreat' in fastFood) {
+    price = fastFood.isGreat() ? 5 : 10;
+  }
+
+  if ('isGreat' in fastFood) {
+    price = fastFood.isGreat() ? 5 : 10;
+  }
+
+  return price;
+}
+
+const pizza = new Pizza();
+buyFastFood(pizza);
+
+//---------- 4. Equality narrow
+
+function getValues(a: number | string, b: string) {
+  if (a === b) {
+    console.log(a);
+  } else {
+    console.log(b);
+  }
+}
+getValues('this is a', 'this is b');
+
+//---------- 5. user-defined Guard
+
+class IBanana {
+  isTasty(): boolean {
+    return true;
+  }
+}
+
+class IApple {
+  isJuicy(): boolean {
+    return true;
+  }
+}
+
+type IFruit = IBanana | IApple;
+
+function isBanana(fruit: IFruit): fruit is IBanana {
+  return fruit instanceof IBanana;
+}
+
+function buyFruitTwo(fruit: IFruit): number {
+  let price = 0;
+
+  if (isBanana(fruit)) {
+    price = fruit.isTasty() ? 5 : 10;
+  } else {
+    price = fruit.isJuicy() ? 5 : 10;
+  }
+
+  return price;
+}
+
+const appleTwo = new Apple();
+buyFruitTwo(appleTwo);
